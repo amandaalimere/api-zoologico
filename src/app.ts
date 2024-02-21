33ac7945c35a4b4b4ea96b123c4e0b3d9c4b4a03
amandaalimere <amanda.mere@aluno.senai.br>
2024-02-21 15:43:57 -0300

    challenge #2 - 21/02/2024

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { exitCode } from "process";
-import { Animal } from "./model/Animal";
-import { Atracao } from "./model/Atracao";
-import { Ave } from "./model/Ave";
 import { Habitat } from "./model/Habitat";
-import { Mamifero } from "./model/Mamifero";
+import { Atracao } from "./model/Atracao";
+import { Zoologico } from "./model/Zoologico";
+import { DatabaseModel } from "./model/DatabaseModel";
 import { Reptil } from "./model/Reptil";
-import  { Zoologico } from "./model/Zoologico";
+import { Mamifero } from "./model/Mamifero";
+import { Ave } from "./model/Ave";
+
 
 const server = express();
 const port: number = 3000;
@@ -16,42 +16,99 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-    let ave: Ave= new Ave(10, 'Calopsita', 11, 'Masculino');
-    let reptil: Reptil= new Reptil ('placóides', 'Cobra', 15, 'Masculino');
-    let mamifero: Mamifero= new Mamifero ('canguru', 'Julia', 12, 'Feminino');
- 
-     res.json([ave,reptil, mamifero]);
- });
- 
- server.post('/ave', (req,res) => {
-     const{ envergadura, nome, idade, genero} = req.body;
-     let ave: Ave= new Ave(envergadura, nome, idade, genero);
-     res.json(["A nova ave do zoologico é:", ave]);
- });
- 
- server.post('/habitat', (req, res) => {
-     const {nome, animais}= req.body;
-     const habitat = new Habitat(nome, animais);
-     console.log(habitat)
-     res.status(200).json('Habitat criado');
- });
- 
- server.post('/atracao', (req, res) => {
-     const {nome, habitat}= req.body;
-     const atracao = new Atracao(nome, habitat);
-     console.log(atracao)
-     res.status(200).json('Atrção criada');
- });
- 
- server.post('/zoologico', (req, res) => {
-     const {nome, atracao}= req.body;
-     const zoo = new Zoologico (nome, atracao);
-     console.log(zoo);
-     res.status(200).json('Zoologico criado');
- });
- 
- 
- 
- server.listen(port, () => {
-     console.log(`Servidor rodando em http://localhost:${port}`);
- })
+    res.json("ola");
+});
+
+server.post('/habitat', (req, res) => {
+    const { nome, animais } = req.body;
+    const habitat = new Habitat(nome, animais);
+    console.log(habitat);
+    res.status(200).json('Habitat criado');
+});
+
+server.post('/atracao', (req, res) => {
+    const { nome, habitat } = req.body;
+    const atracao = new Atracao(nome, habitat);
+    console.log(atracao);
+    res.status(200).json('Atração criada');
+});
+
+server.post('/zoologico', (req, res) => {
+    const { nome, atracao } = req.body;
+    const zoo = new Zoologico(nome, atracao);
+    console.log(zoo);
+    res.status(200).json('Zoológico criado');
+});
+
+server.get('/list/reptil', async (req, res) => {
+    const repteis = await Reptil.listarRepteis();
+
+    res.status(200).json(repteis);
+})
+
+server.post('/new/reptil', async (req, res) => {
+    const { nome, idade, genero, tipo_de_escamas } = req.body;
+
+    const novoReptil = new Reptil(nome, idade, genero, tipo_de_escamas);
+
+    const result = await Reptil.cadastrarReptil(novoReptil);
+
+    if(result) {
+        return res.status(200).json('Reptil cadastrado com sucesso');
+    } else {
+        return res.status(400).json('Não foi possível cadastrar o réptil no banco de dados');
+    }
+    
+})
+
+server.get('/list/mamifero', async (req, res) => {
+    const mamiferos = await Mamifero.listarMamiferos();
+
+    res.status(200).json(mamiferos);
+});
+
+server.post('/new/mamifero', async (req, res) => {
+    const { nome, idade, genero, raca } = req.body;
+
+    const novoMamifero = new Mamifero(nome, idade, genero, raca);
+
+    const result = await Mamifero.cadastrarMamifero(novoMamifero);
+
+    if (result) {
+        return res.status(200).json('Mamífero cadastrado com sucesso');
+    } else {
+        return res.status(400).json('Não foi possível cadastrar o mamífero no banco de dados');
+    }
+});
+
+server.get('/list/ave', async (req, res) => {
+    const aves = await Ave.listarAves();
+
+    res.status(200).json(aves);
+});
+
+server.post('/new/ave', async (req, res) => {
+    const { nome, idade, genero, envergadura } = req.body;
+
+    const novaAve = new Ave(nome, idade, genero, envergadura);
+
+    const result = await Ave.cadastrarAve(novaAve);
+
+    if(result) {
+        return res.status(200).json('Ave cadastrada com sucesso');
+    } else {
+        return res.status(400).json('Não foi possível cadastrar a ave no banco de dados');
+    }
+});
+
+
+
+new DatabaseModel().testeConexao().then((resbd) => {
+    if(resbd) {
+        server.listen(port, () => {
+            console.log(`Servidor rodando em http://localhost:${port}`);
+        })
+    } else {
+        console.log('Não foi possível conectar ao banco de dados');
+    }
+})
--- src/model/Mamifero.ts
+++ src/model/Mamifero.ts
@@ -1,9 +1,12 @@
 import { Animal } from "./Animal";
+import { DatabaseModel } from "./DatabaseModel";
+
+const database = new DatabaseModel().pool;
 
 /**
  * Classe que representa um Mamífero, uma extensão da classe Animal.
  */
-class Mamifero extends Animal {
+export class Mamifero extends Animal {
 
     /**
      * Propriedade privada que armazena a raça do mamífero.
@@ -39,5 +42,40 @@ class Mamifero extends Animal {
      */
     public setRaca(raca: string): void {
         this.raca = raca;
+    }
+
+    static async listarMamiferos() {
+        const listaDeMamiferos: Array<Mamifero> = [];
+        try {
+            const queryReturn = await database.query(`SELECT * FROM  repti`);
+            queryReturn.rows.forEach(reptil => {
+                listaDeMamiferos.push(reptil);
+            });
+
+            // só pra testar se a lista veio certa do banco
+            console.log(listaDeMamiferos);
+
+            return listaDeMamiferos;
+        } catch (error) {
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error";
+        }
+    }
+    static async cadastrarMamifero(mamifero: Mamifero): Promise<any> {
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO mamifero (nome, idade, genero, raca)
+                VALUES
+                ('${mamifero.getNome().toUpperCase()}', ${mamifero.getIdade()}, '${mamifero.getGenero().toUpperCase()}', '${mamifero.getRaca().toUpperCase()}');
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            return insertResult;
+        } catch(error) {
+            return error;
+        }
     }
 }
